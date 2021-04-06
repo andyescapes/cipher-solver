9d319a62231e9281e615897de2bad44ce5b552a8
unknown <65060066+andyescapes@users.noreply.github.com>
2021-04-06 20:00:14 +1000

    frontend working

--- src/App.js
+++ src/App.js
@@ -1,75 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/WordColumn';
-import React, { useState } from 'react';
-import WordColumn from './components/WordColumn';
-import KeyInput from './components/KeyInput';
-import {caesar} from './helper/caesar'
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/WordColumn";
+import React, { useState } from "react";
+import WordColumn from "./components/WordColumn";
+import KeyInput from "./components/KeyInput";
 
 function App() {
-  
-  const [inputWord, setInputWord] = useState([])
-  console.log(inputWord)
-  const alpha = Array.from({ length: 26 }, (_, i) => String.fromCharCode('A'.charCodeAt(0) + i));
+  const [inputWord, setInputWord] = useState([]);
+  const [caesarResult, setCaesarResult] = useState([]);
 
+  console.log(inputWord);
+  const alpha = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode("A".charCodeAt(0) + i)
+  );
 
-  const caesar_solve = (input) =>{
-    if(input && document.getElementById("message")){
-      //const res_arr = caesar(input.join(""))
-      const message =  document.getElementById("message")
-      message.innertext = "xcv"
-      // res_arr.map(res=> {
-      //   message.innerText += `${res} `
-      // })
-      //console.log(res_arr)
+  const caesar_solve = (input) => {
+    const phrase = input.join("");
+    console.log(phrase);
 
-    }
-  }
+    const call_solve = async (input) => {
+      const body = {
+        phrase: input,
+      };
+
+      const result = await fetch("http://localhost:3001/caesar", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      console.log(result);
+      const res_json = await result.json();
+      console.log(res_json);
+      setCaesarResult(res_json.result);
+    };
+    call_solve(phrase);
+  };
 
   return (
-    
     <div className="App">
-      
-      <h1>
-        Cipher Solver
-      </h1>
-      <input placeholder = {"Type in your Cipher"} id ="input">
-      </input>
-      <button onClick = {() =>{
-        setInputWord(document.getElementById("input").value.split(""))
-        //document.getElementById("blanks").innerText = document.getElementById("input").value.replace(/\S/g, "-")
-        
-        }}>Enter</button>
-
-      <p id = "blanks"></p>
-      <p id = "cipher"></p>
-      <div className = "letter_container">
-        {inputWord.map( char =>(
-          <WordColumn letter={char}></WordColumn>
-        )
-            
-            
-          )}
-      </div>
-      <h2>
-        Key:
-      </h2>
-      <div className = "key_container">
-      {
-        alpha.map(alpha_letter =>{
-          return  <KeyInput letter = {alpha_letter}></KeyInput>
-        })
-      }
-      </div>
-      
-      <button onClick = {caesar_solve(inputWord)}>
-        Caesar Solve
+      <h1>Cipher Solver</h1>
+      <input placeholder={"Type in your Cipher"} id="input"></input>
+      <button
+        onClick={() => {
+          setInputWord(document.getElementById("input").value.split(""));
+          //document.getElementById("blanks").innerText = document.getElementById("input").value.replace(/\S/g, "-")
+        }}
+      >
+        Enter
       </button>
-      <p id = "message"></p>
 
-      
+      <p id="blanks"></p>
+      <p id="cipher"></p>
+      <div className="letter_container">
+        {inputWord.map((char) => (
+          <WordColumn letter={char}></WordColumn>
+        ))}
+      </div>
+      <h2>Key:</h2>
+      <div className="key_container">
+        {alpha.map((alpha_letter) => {
+          return <KeyInput letter={alpha_letter} key={alpha_letter}></KeyInput>;
+        })}
+      </div>
+
+      <button onClick={() => caesar_solve(inputWord)}>Caesar Solve</button>
+
+      {caesarResult.map((res) => (
+        <p>{res}</p>
+      ))}
     </div>
   );
 }
 
 export default App;
+//https://www.npmjs.com/package/esm
+//https://deno.land/
--- src/components/KeyInput.js
+++ src/components/KeyInput.js
@@ -1,59 +1,57 @@
-import '.././App.css';
+import ".././App.css";
 function KeyInput(props) {
+  const highlight_letters = () => {
+    const letters = document.getElementsByClassName("word_column");
 
-    const highlight_letters = () =>{
-        const letters = document.getElementsByClassName("word_column")
-        
-        for (let i = 0; i<letters.length; i++){
-            //console.log(letters[i].lastChild.innerText,props.letter.toLowerCase() )
-            if(letters[i].lastChild.innerText == props.letter.toLowerCase()){
-                letters[i].lastChild.className = "highlight"
-                letters[i].firstChild.className = "highlight"
-
-            }
-            
-        }
+    for (let i = 0; i < letters.length; i++) {
+      //console.log(letters[i].lastChild.innerText,props.letter.toLowerCase() )
+      if (letters[i].lastChild.innerText == props.letter.toLowerCase()) {
+        letters[i].lastChild.className = "highlight";
+        letters[i].firstChild.className = "highlight";
+      }
     }
+  };
 
-    const clear_highlight = () =>{
-        const letters = document.getElementsByClassName("word_column")
-        
-        for (let i = 0; i<letters.length; i++){
-            //console.log(letters[i].lastChild.innerText,props.letter.toLowerCase() )
-            if(letters[i].lastChild.innerText == props.letter.toLowerCase()){
-                letters[i].lastChild.classList.remove("highlight")
-            letters[i].firstChild.classList.remove("highlight")
-            }
-            
-        }
+  const clear_highlight = () => {
+    const letters = document.getElementsByClassName("word_column");
+
+    for (let i = 0; i < letters.length; i++) {
+      //console.log(letters[i].lastChild.innerText,props.letter.toLowerCase() )
+      if (letters[i].lastChild.innerText == props.letter.toLowerCase()) {
+        letters[i].lastChild.classList.remove("highlight");
+        letters[i].firstChild.classList.remove("highlight");
+      }
     }
+  };
 
-    const add_letters = () =>{
-        const letters = document.getElementsByClassName("word_column")
-        
-        for (let i = 0; i<letters.length; i++){
-            //console.log(letters[i].lastChild.innerText,props.letter.toLowerCase() )
-            if(letters[i].lastChild.innerText == props.letter.toLowerCase()){
-                const val = document.getElementById(props.letter).value
-                if (val && val.length === 1) letters[i].firstChild.innerText = val
-            }
-            
-        }
+  const add_letters = () => {
+    const letters = document.getElementsByClassName("word_column");
+
+    for (let i = 0; i < letters.length; i++) {
+      //console.log(letters[i].lastChild.innerText,props.letter.toLowerCase() )
+      if (letters[i].lastChild.innerText == props.letter.toLowerCase()) {
+        const val = document.getElementById(props.letter).value;
+        if (val && val.length === 1) letters[i].firstChild.innerText = val;
+      }
     }
+  };
 
-    return(
-        <>
-            <div className = "key_input">
-            <input maxlength="1" id = {props.letter} style ={{width: "100%"}} onFocus = {highlight_letters} onBlur = {clear_highlight} onChange = {add_letters}>
-            </input>
-            <br>
-            </br>
-            {props.letter}
-        </div>
-        
-        
-        </>
-    )
+  return (
+    <>
+      <div className="key_input">
+        <input
+          maxLength="1"
+          id={props.letter}
+          style={{ width: "100%" }}
+          onFocus={highlight_letters}
+          onBlur={clear_highlight}
+          onChange={add_letters}
+        ></input>
+        <br></br>
+        {props.letter}
+      </div>
+    </>
+  );
 }
 
-export default KeyInput
+export default KeyInput;
