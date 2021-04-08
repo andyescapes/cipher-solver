a9b1f19c9d6a559a86a3942c347c403c18cdac1a
unknown <65060066+andyescapes@users.noreply.github.com>
2021-04-08 21:14:19 +1000

    restore point for bad react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./components/WordColumn";
 import React, { useState } from "react";
 import WordColumn from "./components/WordColumn";
 import KeyInput from "./components/KeyInput";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
   const [inputWord, setInputWord] = useState([]);
   const [caesarResult, setCaesarResult] = useState([]);
+  const [frequency, setFrequency] = useState([]);
 
+  const set_freq_obj = (input) => {
+    console.log(input);
+    const freq_list = [];
+    const input_set = new Set(inputWord);
+
+    input_set.forEach((letter) => {
+      let count = 0;
+      input.forEach((letter2) => {
+        if (letter === letter2) {
+          count++;
+        }
+      });
+
+      if (letter != " ") {
+        const frequence_object = { letter: letter };
+        frequence_object.count = count;
+        freq_list.push(frequence_object);
+      }
+    });
+    freq_list.sort((a, b) => parseFloat(b.count) - parseFloat(a.count));
+    setFrequency(freq_list);
+  };
+
+  React.useEffect(() => {
+    set_freq_obj(inputWord);
+  }, [inputWord]);
+
+  console.log(frequency);
   console.log(inputWord);
   const alpha = Array.from({ length: 26 }, (_, i) =>
     String.fromCharCode("A".charCodeAt(0) + i)
@@ -58,8 +92,9 @@ function App() {
           <WordColumn letter={char}></WordColumn>
         ))}
       </div>
-      <h2>Key:</h2>
+
       <div className="key_container">
+        <h2 style={{ display: "inline", margin: "0 10px 0 0" }}>Key: </h2>
         {alpha.map((alpha_letter) => {
           return <KeyInput letter={alpha_letter} key={alpha_letter}></KeyInput>;
         })}
@@ -70,6 +105,53 @@ function App() {
       {caesarResult.map((res) => (
         <p>{res}</p>
       ))}
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Card>
+            <h4>Frequency Analysis</h4>
+            {frequency.map((term) => (
+              <Typography variant="body1" gutterBottom>
+                {term.letter}: {term.count}
+              </Typography>
+            ))}
+            <Container>
+              <Typography variant="body2" gutterBottom>
+                The most frequent letters in english are in the order E, A, R,
+                I, O, T, N, S, L, C
+              </Typography>
+            </Container>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <h4>Single Letters</h4>
+            <Container>
+              <Typography variant="body2" gutterBottom>
+                The single letters are either A or I
+              </Typography>
+              <Button variant="contained" color="primary">
+                Fill A
+              </Button>
+              <Button variant="contained" color="secondary">
+                Fill I
+              </Button>
+            </Container>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <h4>Repeated Letters</h4>
+
+            <Container>
+              <Typography variant="body2" gutterBottom>
+                double letters most likely to be LL and if not EE, SS, OO and TT
+              </Typography>
+            </Container>
+
+            <Button>Fill in</Button>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
